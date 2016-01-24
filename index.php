08804aa034e6faf338aeff20ae4595dbae17d3f5
litbear <litbear@foxmail.com>
2016-01-24 23:42:29 +0800

    一些修改

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 // framework/index.php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/init.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+$input = $request->get('name', 'World');
 
-$request = Request::createFromGlobals();
-
-$input = $request->cookies->get('_ga');
-
-$response = new Response(var_dump($input));
-
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->send();
